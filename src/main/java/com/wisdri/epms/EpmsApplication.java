64d97b315cab4641e3e8d184746d1fda09876bed
unknown <14530@wisdri.com>
2022-03-06 16:37:11 +0800

    添加打包成Windows服务的工具

--- src/main/java/com/wisdri/epms/EpmsApplication.java
+++ src/main/java/com/wisdri/epms/EpmsApplication.java
@@ -2,12 +2,21 @@ package com.wisdri.epms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EpmsApplication {
+public class EpmsApplication //extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpmsApplication.class, args);
 	}
 
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		//参数为当前SpringBoot启动类
+//		//构造新资源
+//		return builder.sources(EpmsApplication.class);
+//	}
 }
