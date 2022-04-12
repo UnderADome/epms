f7a53494681af8c50578eb200cac56997251af8d
UnderADome <mushi2016@outlook.com>
2022-04-12 18:26:33 +0800

    查询LDAP用户信息——临时提交（一）

--- src/main/java/com/wisdri/epms/Util/LDAPUtil.java
+++ src/main/java/com/wisdri/epms/Util/LDAPUtil.java
@@ -1,5 +1,6 @@
 package com.wisdri.epms.Util;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -7,11 +8,15 @@ import org.springframework.stereotype.Service;
 
 import javax.naming.AuthenticationException;
 import javax.naming.Context;
-import javax.naming.directory.DirContext;
-import javax.naming.directory.InitialDirContext;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.*;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 /**
  * @author 李韬 @date 2022/4/7 9:31
@@ -52,7 +57,8 @@ public class LDAPUtil {
         String user = userName.indexOf(domain) > 0 ? userName : userName
                 + domain;//网上有别的方法，但是在我这儿都不好使，建议这么使用
         Hashtable env = new Hashtable();//实例化一个Env
-        DirContext ctx = null;
+        LdapContext ctx = null;
+        //DirContext ctx = null;
         env.put(Context.SECURITY_AUTHENTICATION, "simple");//LDAP访问安全级别(none,simple,strong),一种模式，这么写就行
         env.put(Context.SECURITY_PRINCIPAL, user); //用户名
         env.put(Context.SECURITY_CREDENTIALS, password);//密码
@@ -60,7 +66,10 @@ public class LDAPUtil {
                 "com.sun.jndi.ldap.LdapCtxFactory");// LDAP工厂类
         env.put(Context.PROVIDER_URL, url);//Url
         try {
-            ctx = new InitialDirContext(env);// 初始化上下文
+            ctx = new InitialLdapContext(env, null);
+            //ctx = new InitialDirContext(env);// 初始化上下文
+            log.info("初始化ctx");
+            readLdap(ctx, "dc=wisdri,dc=com");
             log.info(LocalDateTime.now() + " " + userName + " " + password + " " + "身份验证成功!");
             return true;
         } catch (AuthenticationException e) {
@@ -85,5 +94,82 @@ public class LDAPUtil {
                 }
             }
         }
+    }
+
+    public void readLdap(LdapContext ctx, String basedn){
+        List<LdapUser> lm=new ArrayList<LdapUser>();
+        try{
+            if (ctx!=null){
+                log.info("进入readLdap方法");
+                //过滤条件
+                String filter = "uid=14530";//"(&(objectClass=*)(uid=*))"
+                String[] attrPersonArray = { "uid", "userPassword", "displayName", "cn", "sn", "mail", "description" };
+                //搜索控件
+                SearchControls searchControls = new SearchControls();
+                searchControls.setSearchScope(2);//搜索范围
+                searchControls.setReturningAttributes(attrPersonArray);
+                //测试
+                NamingEnumeration answer_test = ctx.search(basedn, filter.toString(),searchControls);
+                while(answer_test.hasMoreElements()){
+                    SearchResult sr = (SearchResult) answer_test.next();
+                    log.info("getname=" + sr.getName());
+                }
+                //https://blog.csdn.net/belialxing/article/details/89157737
+                //https://www.cnblogs.com/Nadim/p/4681003.html
+                //https://blog.csdn.net/qq_34605063/article/details/108303657
+                // 1.要搜索的上下文或对象的名称；
+                // 2.过滤条件，可为null，默认搜索所有信息；
+                // 3.搜索控件，可为null，使用默认的搜索控件
+                NamingEnumeration<SearchResult> answer = ctx.search(basedn, filter.toString(),searchControls);
+                log.info("搜索结果是否为空？" + (answer == null) + " hasMore?" + (answer.hasMore()) + "  " + answer.toString());
+                //while (answer.hasMore()) {
+                    log.info("执行搜索ing...");
+                    SearchResult result = (SearchResult) answer.next();
+                    NamingEnumeration<? extends Attribute> attrs = result.getAttributes().getAll();
+                    LdapUser lu=new LdapUser();
+                    while (attrs.hasMore()) {
+                        Attribute attr = (Attribute) attrs.next();
+                        if("userPassword".equals(attr.getID())){
+                            Object value = attr.get();
+                            lu.setUserPassword(new String((byte [])value));
+                        }else if("uid".equals(attr.getID())){
+                            lu.setUid(attr.get().toString());
+                        }else if("displayName".equals(attr.getID())){
+                            lu.setDisplayName(attr.get().toString());
+                        }else if("cn".equals(attr.getID())){
+                            lu.setCn(attr.get().toString());
+                        }else if("sn".equals(attr.getID())){
+                            lu.setSn(attr.get().toString());
+                        }else if("mail".equals(attr.getID())){
+                            lu.setMail(attr.get().toString());
+                        }else if("description".equals(attr.getID())){
+                            lu.setDescription(attr.get().toString());
+                        }
+                        System.out.println(lu);
+                    }
+                    if(lu.getUid()!=null)
+                        lm.add(lu);
+                //}
+            }
+        }catch (Exception e) {
+            System.out.println("获取用户信息异常:");
+            e.printStackTrace();
+        }
+
+        //return lm;
+    }
+
+    @Data
+    public class LdapUser {
+        public String cn;
+        public String sn;
+        public String uid;
+        public String userPassword;
+        public String displayName;
+        public String mail;
+        public String description;
+        public String uidNumber;
+        public String gidNumber;
+        /**忽略get\set方法**/
     }
 }
